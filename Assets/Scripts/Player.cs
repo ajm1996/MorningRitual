0eae7e3ed3f3f2c6e6fc0017ac2b6c06c6cf9ed9
Austin Miller <mill1751@purdue.edu>
2016-01-29 21:25:05 -0500

    Added placeholders and player controls
    Added standard WASD controls and a placeholder for the ground and the
    player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public int playerSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float movex = Input.GetAxis ("Horizontal");
+		float moveY = Input.GetAxis ("Vertical");
+		gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (movex * playerSpeed, moveY * playerSpeed));
+
+	}
+}
