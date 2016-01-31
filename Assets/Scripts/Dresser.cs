9db4b634b93ae378901da06fdc4d191a3cc6e98a
Austin Miller <mill1751@purdue.edu>
2016-01-31 12:19:11 -0500

    Added house scene back, added dresser and some small fixes

--- Assets/Scripts/Dresser.cs
+++ Assets/Scripts/Dresser.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class Sink : MonoBehaviour {
+public class Dresser : MonoBehaviour {
 
 	Slider slide;
 	bool inRange;
--- Assets/Scripts/Shower.cs
+++ Assets/Scripts/Shower.cs
@@ -17,24 +17,6 @@ public class Shower : MonoBehaviour {
 	void Update () {
 		if (inRange) {
 			if (slide.value < slide.maxValue) {
-<<<<<<< HEAD
-				slide.value += .005F;
-			}
-			if (slide.value == slide.maxValue) {
-				if (doOnce < 1) {
-					announce.getAnnouncements ("Your balls are lathered");
-					doOnce++;
-				}
-				ShowerSlider.SetActive (false);
-			}
-		} else {
-			if (slide.value < slide.maxValue) {
-				if(slide.value == 0){
-				}else{
-					slide.value -= .005F;
-				}
-			}
-=======
 				slide.value += (1/10f) * Time.deltaTime;
 			}
 			if (slide.value >= slide.maxValue) {
@@ -43,7 +25,6 @@ public class Shower : MonoBehaviour {
 			} 
 		} else if (slide.value > 0) {
 			slide.value -= (1/10f) * Time.deltaTime;
->>>>>>> origin/master
 		}
 	}
 
