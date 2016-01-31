3541eb5bcf1fcd4682f72f175e91c5a521b2b4d2
EverettG <egwyn@purdue.edu>
2016-01-30 23:34:00 -0500

    Added Shower Prefab

---
+++ Assets/Scripts/Shower.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Shower : MonoBehaviour {
+
+	Slider slide;
+	GameObject ShowerSlider; 
+	Announcement announce;
+	int doOnce;
+	bool inRange;
+	// Use this for initialization
+	void Start () {
+		doOnce = 0;
+		slide = GetComponentInChildren<Slider>();
+		ShowerSlider = slide.transform.parent.gameObject;
+		announce = GameObject.Find ("Announce text").GetComponentInChildren<Announcement> ();
+		inRange = false;
+		ShowerSlider.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(inRange){
+			if (slide.value < slide.maxValue) {
+				slide.value += .005F;
+			}
+			if(slide.value == slide.maxValue){
+				if (doOnce < 1) {
+					announce.getAnnouncements ("Your balls are lathered");
+					doOnce++;
+				}
+				ShowerSlider.SetActive (false);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == "Player") {
+			inRange = true;
+			ShowerSlider.SetActive (true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if (col.gameObject.tag == "Player") {
+			if (slide.value < slide.maxValue) {
+				slide.value -= .1F;
+			}
+			inRange = false;
+			ShowerSlider.SetActive (false);
+		}
+	}
+}
